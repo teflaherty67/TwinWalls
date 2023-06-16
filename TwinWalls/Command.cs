abdf135d4dfc92eb76bcd8a1af1e47239759cf6b
Tom E Flaherty <teflaherty@live.com>
2023-06-16 15:50:43 -0500

    just started

--- TwinWalls/Command.cs
+++ TwinWalls/Command.cs
@@ -26,25 +26,59 @@ namespace TwinWalls
             Application app = uiapp.Application;
             Document curDoc = uidoc.Document;
 
-           // prompt the user to select the walls to "twin"
+            // prompt the user to select the walls to "twin"
 
-           // verify all selected elements are walls
+            // verify all selected elements are walls
 
-           // set the Location Line for all selected walls to Core Face: Exterior
+            // set the Location Line for all selected walls to Core Face: Exterior
 
-           // if wall name contains 2x4, new interior wall to be 2x4,GWB
+            // if wall name contains 2x4, new interior wall to be 2x4,GWB
 
-           // if wall name contains 2x6, new interior wall to be 2x6,GWB
+            // if wall name contains 2x6, new interior wall to be 2x6,GWB
 
 
+
+            // Get the current Revit application and document
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            // Prompt the user to select walls
+            ReferenceList referenceList = uiDoc.Selection.PickObjects(ObjectType.Element, new WallSelectionFilter(), "Select walls");
+
+            // Create a selection set and add the selected walls
+            Selection sel = uiDoc.Selection;
+            sel.Elements.Clear();
+            foreach (Reference reference in referenceList)
+            {
+                Element element = doc.GetElement(reference);
+                sel.Elements.Add(element);
+            }
 
             return Result.Succeeded;
+
         }
+
+        // Custom selection filter to select only walls
+        public class WallSelectionFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element elem)
+            {
+                return elem is Wall;
+            }
+
+            public bool AllowReference(Reference reference, XYZ position)
+            {
+                return false;
+            }
+        }
+
 
         public static String GetMethod()
         {
             var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
             return method;
         }
+
     }
 }
