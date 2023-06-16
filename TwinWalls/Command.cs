4eb1fb20fcd31b70508e54be560a2c4bca712e42
Tom E Flaherty <teflaherty@live.com>
2023-06-16 15:08:53 -0500

    Add project files.

---
+++ TwinWalls/Command.cs
@@ -0,0 +1,50 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+
+#endregion
+
+namespace TwinWalls
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document curDoc = uidoc.Document;
+
+           // prompt the user to select the walls to "twin"
+
+           // verify all selected elements are walls
+
+           // set the Location Line for all selected walls to Core Face: Exterior
+
+           // if wall name contains 2x4, new interior wall to be 2x4,GWB
+
+           // if wall name contains 2x6, new interior wall to be 2x6,GWB
+
+
+
+            return Result.Succeeded;
+        }
+
+        public static String GetMethod()
+        {
+            var method = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+            return method;
+        }
+    }
+}
